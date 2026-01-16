70e7440d6ddb0239fbacf57bbe4fb337c503e253
Matteo Gassend <mgassend@gmail.com>
2026-01-16 17:23:40 +0100

    fix: repo sync url now depends on env var

--- apps/web/src/sync/repo.ts
+++ apps/web/src/sync/repo.ts
@@ -4,9 +4,11 @@ import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network
 import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb";
 
 export const repo = new Repo({
-	storage: new IndexedDBStorageAdapter(),
-	network: [
-		new BroadcastChannelNetworkAdapter(),
-		new BrowserWebSocketClientAdapter("ws://localhost:3000/automerge"),
-	],
+  storage: new IndexedDBStorageAdapter(),
+  network: [
+    new BroadcastChannelNetworkAdapter(),
+    new BrowserWebSocketClientAdapter(
+      `ws://${import.meta.env.VITE_API_URL}/automerge`,
+    ),
+  ],
 });
